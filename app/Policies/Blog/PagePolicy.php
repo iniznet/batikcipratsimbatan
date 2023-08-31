9f1c40a2c0cc0c5c4dbc7d174d93fe7219ec256e
iniznet <iniznet@gmail.com>
2023-08-31 22:00:49 +0700

    feat: role & policies

---
+++ app/Policies/Blog/PagePolicy.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Policies\Blog;
+
+use App\Models\Management\User;
+use App\Models\Blog\Page;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PagePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can('view_any_blog::page');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @param  \App\Models\Blog\Page  $page
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, Page $page): bool
+    {
+        return $user->can('view_blog::page');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user): bool
+    {
+        return $user->can('create_blog::page');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @param  \App\Models\Blog\Page  $page
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, Page $page): bool
+    {
+        return $user->can('update_blog::page');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @param  \App\Models\Blog\Page  $page
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, Page $page): bool
+    {
+        return $user->can('delete_blog::page');
+    }
+
+    /**
+     * Determine whether the user can bulk delete.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function deleteAny(User $user): bool
+    {
+        return $user->can('delete_any_blog::page');
+    }
+
+    /**
+     * Determine whether the user can permanently delete.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @param  \App\Models\Blog\Page  $page
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, Page $page): bool
+    {
+        return $user->can('force_delete_blog::page');
+    }
+
+    /**
+     * Determine whether the user can permanently bulk delete.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDeleteAny(User $user): bool
+    {
+        return $user->can('force_delete_any_blog::page');
+    }
+
+    /**
+     * Determine whether the user can restore.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @param  \App\Models\Blog\Page  $page
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, Page $page): bool
+    {
+        return $user->can('restore_blog::page');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restoreAny(User $user): bool
+    {
+        return $user->can('restore_any_blog::page');
+    }
+
+    /**
+     * Determine whether the user can replicate.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @param  \App\Models\Blog\Page  $page
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function replicate(User $user, Page $page): bool
+    {
+        return $user->can('replicate_blog::page');
+    }
+
+    /**
+     * Determine whether the user can reorder.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function reorder(User $user): bool
+    {
+        return $user->can('reorder_blog::page');
+    }
+
+}
---
+++ app/Policies/Management/UserPolicy.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Policies\Management;
+
+use App\Models\Management\User;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->can('view_any_management::user');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user): bool
+    {
+        return $user->can('view_management::user');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user): bool
+    {
+        return $user->can('create_management::user');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user): bool
+    {
+        return $user->can('update_management::user');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user): bool
+    {
+        return $user->can('delete_management::user');
+    }
+
+    /**
+     * Determine whether the user can bulk delete.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function deleteAny(User $user): bool
+    {
+        return $user->can('delete_any_management::user');
+    }
+
+    /**
+     * Determine whether the user can permanently delete.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user): bool
+    {
+        return $user->can('force_delete_management::user');
+    }
+
+    /**
+     * Determine whether the user can permanently bulk delete.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDeleteAny(User $user): bool
+    {
+        return $user->can('force_delete_any_management::user');
+    }
+
+    /**
+     * Determine whether the user can restore.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user): bool
+    {
+        return $user->can('restore_management::user');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restoreAny(User $user): bool
+    {
+        return $user->can('restore_any_management::user');
+    }
+
+    /**
+     * Determine whether the user can bulk restore.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function replicate(User $user): bool
+    {
+        return $user->can('replicate_management::user');
+    }
+
+    /**
+     * Determine whether the user can reorder.
+     *
+     * @param  \App\Models\Management\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function reorder(User $user): bool
+    {
+        return $user->can('reorder_management::user');
+    }
+}
