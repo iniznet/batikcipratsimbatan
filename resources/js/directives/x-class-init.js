d2281a08371e0984be770b9cc4d5994a22c37b99
iniznet <iniznet@gmail.com>
2023-09-24 11:07:29 +0700

    fix(front-js): fix directive

--- resources/js/directives/x-class-init.js
+++ resources/js/directives/x-class-init.js
@@ -1,20 +1,20 @@
 export default (el, {modifiers, expression}, {evaluate}) => {
     const delayIndex = modifiers.findIndex(mod => mod === 'delay');
-    const delayMs = delayIndex > -1 ? parseInt(modifiers[delayIndex + 1], 10) : 300;
+    const delayMs = delayIndex > -1 ? parseInt(modifiers[delayIndex + 1], 10) : 0;
 
     const holdIndex = modifiers.findIndex(mod => mod === 'hold');
     const holdMs = holdIndex > -1 ? parseInt(modifiers[holdIndex + 1], 10) : 300;
 
-    const classes = evaluate(expression);
-
-    const addClasses = () => {
-        el.classList.add(...classes);
-    }
+    const classes = (evaluate(expression)).split(' ');
 
     const removeClasses = () => {
         el.classList.remove(...classes);
     }
 
+    const addClasses = () => {
+        el.classList.add(...classes);
+        setTimeout(removeClasses, holdMs);
+    }
+
     setTimeout(addClasses, delayMs);
-    setTimeout(removeClasses, holdMs);
 }
