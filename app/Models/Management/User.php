b64bea6caf1b99cad204f8d2e7fd555e361f87f6
iniznet <iniznet@gmail.com>
2023-09-25 01:09:39 +0700

    refactor(model): implement filament user

--- app/Models/Management/User.php
+++ app/Models/Management/User.php
@@ -5,13 +5,16 @@ namespace App\Models\Management;
 use App\Models\Blog\Page;
 use App\Models\Blog\Post;
 use App\Models\Shop\Product;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Rappasoft\LaravelAuthenticationLog\Traits\AuthenticationLoggable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable, HasRoles, AuthenticationLoggable;
 
@@ -46,23 +49,28 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function posts()
+    public function posts(): HasMany
     {
         return $this->hasMany(Post::class, 'author_id');
     }
 
-    public function pages()
+    public function pages(): HasMany
     {
         return $this->hasMany(Page::class, 'author_id');
     }
 
-    public function products()
+    public function products(): HasMany
     {
         return $this->hasMany(Product::class, 'author_id');
     }
 
-    public function comments()
+    public function comments(): HasMany
     {
         return $this->hasMany(Comment::class, 'author_id');
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }
