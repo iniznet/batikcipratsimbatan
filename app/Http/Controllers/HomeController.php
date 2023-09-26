b512b5ad3bdcf93ddadcf384bc81b9b504202938
iniznet <iniznet@gmail.com>
2023-09-26 12:46:18 +0700

    refactor(routing): re-add middleware group

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use SmashedEgg\LaravelRouteAnnotation\Route;
 
+#[Route(middleware: ['web'])]
 class HomeController extends Controller
 {
     #[Route('/', name: 'home', methods: ['GET'])]
