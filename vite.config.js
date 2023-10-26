a087e3f166b77d4aeaa5d1ba38f2bb24d740fab9
iniznet <iniznet@gmail.com>
2023-10-26 23:56:34 +0700

    refactor: vite inputs

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,11 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin/theme.css'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/css/filament/admin/theme.css',
+            ],
             refresh: true,
         }),
     ],
