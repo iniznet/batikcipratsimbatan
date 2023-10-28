1280a3e5f2a13611fbd8f0d7a1ff5fb55310de66
iniznet <iniznet@gmail.com>
2023-10-28 14:34:46 +0700

    feat: footer

---
+++ resources/views/sections/footer.blade.php
@@ -0,0 +1,81 @@
+<footer class="relative bg-[#f5f7fd] pt-16 pb-9">
+  {{-- Container --}}
+  <div class="relative z-10 px-5 md:px-8 lg:px-10 max-w-[1440px] xl:px-24 2xl:max-w-full mx-auto">
+    {{-- Content --}}
+    <div class="lg:grid grid-cols-7 space-y-4">
+      {{-- Left --}}
+      <div class="relative col-span-3 space-y-4 pr-8">
+        {{-- Logo --}}
+        <div
+          x-data
+          x-class-init.delay.1000.hold.2000="'scale-110'"
+          class="inline-flex justify-start transition duration-1000 font-title place-self-start hover:scale-110"
+          >
+          <a href="/" class="text-center">
+            <span class="block text-3xl font-semibold leading-normal lg:text-4xl">Batik Ciprat</span>
+            <span class="block -mt-4 text-2xl font-light leading-normal">Langitan</span>
+          </a>
+        </div>
+
+        {{-- Tagline --}}
+        <div class="text-xl">
+          <p>Batik Ciprat Langitan Simbatan — adalah sebuah komunitas yang bergerak di bidang batik diperuntukkan bagi masyarakat difabel.</p>
+        </div>
+      </div>
+
+      {{-- Right --}}
+      <div class="col-start-4 col-end-8 space-y-4">
+        {{-- Footer Navigation --}}
+        <div class="grid pb-6 gap-y-4 md:grid-cols-2">
+          {{-- Left --}}
+          <div class="text-xl">
+            <div class="mb-4 font-semibold">Navigasi</div>
+            <ul class="flex flex-col space-y-2">
+              <li>
+                <a href="/">Beranda</a>
+              </li>
+              <li>
+                <a href="/blog">Blog</a>
+              </li>
+              <li>
+                <a href="/katalog">Katalog</a>
+              </li>
+            </ul>
+          </div>
+
+          {{-- Right --}}
+          <div class="text-xl">
+            <div class="mb-4 font-semibold">Lainnya</div>
+            <ul class="flex flex-col space-y-2">
+              <li>
+                <a href="/page/tentang-kami">Tentang Kami</a>
+              </li>
+              <li>
+                <a href="/page/kebijakan-privasi">Kebijakan Privasi</a>
+              </li>
+              <li>
+                <a href="/page/faq">FAQ</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {{-- Copyright & Credit --}}
+        <div class="grid md:grid-cols-2 pt-6 border-t border-t-[#a5b5e6]">
+          {{-- Left --}}
+          <div>@ 2023 Batik Ciprat Langitan Simbatan.<br> All Rights Reserved.</div>
+
+          {{-- Right --}}
+          <div>Dikembangkan oleh <a href="https://github.com/iniznet/" target="_blank" rel="noopener noreferrer">Tim PPK Deptics 2023</a></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+{{-- Load FancyBox --}}
+<div
+  x-ignore
+  ax-load
+  x-data="iframe"
+/>
